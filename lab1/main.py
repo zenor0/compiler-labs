c48d0931d782cd3ac5cc4e2e5bce6ed53309ba79
zenor0 <zenor0@outlook.com>
2024-03-29 14:42:36 +0800

    feat: add cli args parser

--- lab1/main.py
+++ lab1/main.py
@@ -1,22 +1,57 @@
 import sys
 from lexical import LexicalParser, _TOKEN_TYPE
-import warnings
+import argparse
+import logging
+
+# set logging format
+logging.basicConfig(format='%(levelname)s: \t%(message)s')
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f'Usage: python3 {sys.argv[0]} <filename>')
+    ap = argparse.ArgumentParser(description='Lexical Parser')
+    
+    ap.add_argument('filename', type=str, help='input file')
+    ap.add_argument('-v', '--verbose', action='store_true', help='verbose mode')
+    
+    args = ap.parse_args()
+    filename = args.filename
+    verbose_mode = args.verbose
+    
+    if verbose_mode:
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.debug("Verbose mode enabled.")
+        logging.debug(f"Input file: {filename}")
+        logging.debug("-"*20)
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+    
+    try:
+        with open(filename, "r") as f:
+            code = f.read()
+            logging.info(f"File '{filename}' loaded.")
+    except FileNotFoundError:
+        logging.error(f"File '{filename}' not found.")
         sys.exit(1)
-
-    filename = sys.argv[1]
-    with open(filename, "r") as f:
-        code = f.read()
+    
+    
+    # start to parse code
     parser = LexicalParser(code)
     tokens = parser.parse()
+    
+    if verbose_mode:
+        logging.debug("-"*20)
+        logging.debug("Tokens: ")
+        logging.debug("-"*20)
+        for token in tokens:
+            logging.debug(token)
+        logging.debug("-"*20)
 
     # save to file
     with open(filename+'.out', "w") as f:
         for token in tokens:
             f.write(str(token)+"\n")
+    logging.info(f"Tokens saved to {filename}.out")
+    
     # print symbol table
     keyword_list = {}
     identifier_list = {}
@@ -36,3 +71,20 @@ if __name__ == "__main__":
             f.write(f'IDENTIFIER {i}\n')
         for n in number_list:
             f.write(f'NUMBER {n}\n')
+    logging.info(f"Symbol table saved to {filename}.sym")
+            
+    if verbose_mode:
+        logging.debug("-"*20)
+        logging.debug("Symbol Table: ")
+        logging.debug("-"*20)
+        for k in keyword_list:
+            logging.debug(f'KEYWORD\t\t {k}')
+        for i in identifier_list:
+            logging.debug(f'IDENTIFIER\t {i}')
+        for n in number_list:
+            logging.debug(f'NUMBER\t\t {n}')
+        
+        logging.debug("-"*20)            
+    
+    
+    logging.info("Done. exiting...")
