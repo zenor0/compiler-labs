4f1589556766705a0a5bd0ca06790e63bed00862
zenor0 <zenor0@outlook.com>
2024-04-29 23:49:22 +0800

    feat: support read in `|` grammar

--- lab2/utils/reader.py
+++ lab2/utils/reader.py
@@ -6,10 +6,10 @@ PRODUCTION_RE = r'\s*(\S+)\s*->\s*(.+)\n'
 
 test_raw = """
 S -> A B | b C
-A -> <epsilon> | A -> b
-B -> <epsilon> | B -> a D
-C -> A D | C -> b
-D -> a S | D -> c
+A -> <epsilon> | b
+B -> <epsilon> | a D
+C -> A D | b
+D -> a S | c
 """
 
 test_grammar = """
@@ -31,9 +31,23 @@ B -> a B
 B -> b
 """
 
+
+import re
+from models import Production, Symbol
+
+OR_PRODUCTION_RE = r'\s*(\S+)\s*->\s*.+([.|\s|\n]*\|\s*)(.+)'
+def parse_raw(raw: str):
+
+    return raw
+
 def read_grammar(raw : str) -> list[Production]:
-    # TO-DO
-    # break down raw string into single productions
+    match = re.findall(OR_PRODUCTION_RE, raw)
+    while match:
+        for m in match:
+            raw = raw.replace(m[1]+m[2], f'\n{m[0]} -> {m[2]}')
+            
+        match = re.findall(OR_PRODUCTION_RE, raw)
+    
     match = re.findall(PRODUCTION_RE, raw)
     
     # create a list of productions
@@ -43,4 +57,5 @@ def read_grammar(raw : str) -> list[Production]:
         body = [Symbol(x) for x in prod[1].split()]
         productions.append(Production(head, body))
     
-    return productions
+    # de-duplicate
+    return [x for i, x in enumerate(productions) if productions.index(x) == i]
