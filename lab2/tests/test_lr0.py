9ec9745b74d8c9f90335928717a3c4e0f5834566
fyvv00 <156162088+fyvv00@users.noreply.github.com>
2024-05-07 10:36:18 +0800

    Add files via upload

--- lab2/tests/test_lr0.py
+++ lab2/tests/test_lr0.py
@@ -1,5 +1,6 @@
-from models import lr0
+from models import lr0,Symbol,Production,Grammar
 from utils import reader
+
 
 def test_lr0():
     grammar = """
@@ -12,3 +13,56 @@ def test_lr0():
     parser = lr0.LR0(production)
     
     # TO-DO
+    expected_productions = [
+        Grammar([
+            Production(Symbol("S'"), [Symbol('·'), Symbol('S')]),
+            Production(Symbol('S'), [Symbol('·'), Symbol('B'), Symbol('B')]),
+            Production(Symbol('B'), [Symbol('·'), Symbol('a'), Symbol('B')]),
+            Production(Symbol('B'), [Symbol('·'), Symbol('b')])
+        ]),
+        Grammar([
+            Production(Symbol("S'"), [Symbol('S'), Symbol('·')])
+        ]),
+        Grammar([
+            Production(Symbol('S'), [Symbol('B'), Symbol('·'), Symbol('B')]),
+            Production(Symbol('B'), [Symbol('·'), Symbol('a'), Symbol('B')]),
+            Production(Symbol('B'), [Symbol('·'), Symbol('b')])
+        ]),
+        Grammar([
+            Production(Symbol('B'), [Symbol('a'), Symbol('·'), Symbol('B')]),
+            Production(Symbol('B'), [Symbol('·'), Symbol('a'), Symbol('B')]),
+            Production(Symbol('B'), [Symbol('·'), Symbol('b')])
+        ]),
+        Grammar([
+            Production(Symbol('B'), [Symbol('b'), Symbol('·')])
+        ]),
+        Grammar([
+            Production(Symbol('S'), [Symbol('B'), Symbol('B'), Symbol('·')])
+        ]),
+        Grammar([
+            Production(Symbol('B'), [Symbol('a'), Symbol('B'), Symbol('·')])
+        ])
+    ]
+    for state, expected_production in zip(parser.states, expected_productions):
+        productions = []
+        for item,production in zip(state.states,expected_production):
+            assert str(production) == str(item)
+    
+
+    expected_state_transition = [
+        (parser.states[0],"S",parser.states[1]),
+        (parser.states[0],"B",parser.states[2]),
+        (parser.states[0],"a",parser.states[3]),
+        (parser.states[0],"b",parser.states[4]),
+        (parser.states[2],"B",parser.states[5]),
+        (parser.states[2],"a",parser.states[3]),
+        (parser.states[2],"b",parser.states[4]),
+        (parser.states[3],"B",parser.states[6]),
+        (parser.states[3],"a",parser.states[3]),
+        (parser.states[3],"b",parser.states[4]),
+
+    ]
+    assert parser.state_transition == expected_state_transition
+
+    #assert parser.state_transition[0]==
+# test_lr0()
