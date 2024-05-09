dac8c6226f1b4bc85fe8d6a981c63170f8b2ea3f
zenor0 <zenor0@outlook.com>
2024-05-09 19:25:55 +0800

    feat: add watch

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 import rich.table
 from lexical import LexicalParser, _TOKEN_TYPE
@@ -10,75 +11,51 @@ from rich.logging import RichHandler
 from rich.columns import Columns
 from rich.syntax import Syntax
 from rich.panel import Panel
+from rich.table import Table
 
-# from rich import logging
-
-# set logging format
 logger = logging.getLogger('rich')
 logger.setLevel(logging.INFO)
 rich_handler = RichHandler()
 logger.addHandler(rich_handler)
 
 def change_file_extension(filename, new_ext):
-    # Split the filename into parts on the '.' character
     parts = filename.rsplit('.', 1)
     
-    # Check if there is an extension to replace
     if len(parts) == 2:
-        # Replace the old extension with the new one
         return parts[0] + '.' + new_ext
     else:
-        # No extension found, add the new one
         return filename + '.' + new_ext
-
-if __name__ == "__main__":
-    ap = argparse.ArgumentParser(description='Lexical Parser')
     
-    ap.add_argument('filename', type=str, help='input file')
-    ap.add_argument('-v', '--verbose', action='store_true', help='verbose mode')
-    ap.add_argument('-o', '--output', type=str, help='output file path', default='./outputs/')
-    ap.add_argument('-s', '--stdout', action='store_true', help='print to stdout')
-    
-    args = ap.parse_args()
-    filename = args.filename
-    
-    if args.verbose:
-        logger.setLevel(logging.DEBUG)
-        logger.debug("Verbose mode enabled.")
-        logger.debug(f"Input file: {filename}")
-    
+def read_code(filename) -> str:
+    logger.debug(f"Read input file: {filename}")
     try:
         with open(filename, "r") as f:
             code = f.read()
             logger.info(f"File '{filename}' loaded.")
-    except FileNotFoundError:
+            return code
+    except:
         logger.error(f"File '{filename}' not found.")
         sys.exit(1)
-    
-    
-    # start to parse code
-    parser = LexicalParser(code)
-    tokens = parser.parse()
-    
-    if args.verbose:
-        logger.debug("Parsing done.")
-        logger.debug("Showing token table:")
-        token_table = rich.table.Table(title="Token Table")
-        token_table.add_column("Type", style="magenta")
-        token_table.add_column("Value", style="green")
-        for token in tokens:
-            token_table.add_row(token.type.name, str(token.value))
-    if args.stdout:
-        for token in tokens:
-            print(f"{token.type.name}: {token.value}")
-    # Save tokens to file
-    token_filename = args.output + change_file_extension(filename.split('/')[-1], 'out')
-    with open(token_filename, "w") as f:
-        for token in tokens:
-            f.write(str(token)+"\n")
-    logger.info(f"Tokens saved to {token_filename}")
-    
-    # print symbol table
+
+def create_token_table(tokens) -> Table:
+    token_table = Table(title="Token Table")
+    token_table.add_column("Type", style="magenta")
+    token_table.add_column("Value", style="green")
+    for token in tokens:
+        token_table.add_row(token.type.name, str(token.value))
+        
+    return token_table
+
+def create_symbol_table(symbol_list: dict):
+    symbol_table = rich.table.Table(title="Symbol Table")
+    symbol_table.add_column("Type", style="magenta")
+    symbol_table.add_column("Value", style="green")
+    for type in symbol_list:
+        for value in symbol_list[type]:
+            symbol_table.add_row(type, value)
+    return symbol_table
+
+def get_symbol_list(tokens):
     keyword_list = {}
     identifier_list = {}
     number_list = {}
@@ -89,35 +66,112 @@ if __name__ == "__main__":
             identifier_list[token.value] = identifier_list.get(token.value, 0) + 1
         elif token.type == _TOKEN_TYPE.NUMBER:
             number_list[token.value] = number_list.get(token.value, 0) + 1
+            
+    symbol_list = {}
+    symbol_list['KEYWORD'] = [k for k in keyword_list if keyword_list[k] > 0]
+    symbol_list['IDENTIFIER'] = [i for i in identifier_list if identifier_list[i] > 0]
+    symbol_list['NUMBER'] = [n for n in number_list if number_list[n] > 0]
+    return symbol_list
+
+def save_symbols_to_file(symbol_list: dict, filename='code.sym'):
+    with open(filename, "w") as f:
+        for type in symbol_list:
+            for value in symbol_list[type]:
+                f.write(f'{type} {value}\n')
+    logger.info(f"Symbols saved to {filename}")
+
+def save_tokens_to_file(tokens, filename='code.out'):
+    with open(filename, "w") as f:
+        for token in tokens:
+            f.write(str(token)+"\n")
+    logger.info(f"Tokens saved to {filename}")
+
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class WatchHandler(FileSystemEventHandler):
+    def __init__(self, output_path):
+        super().__init__()
+        self.output_path = output_path
+        self._modified_times = {}
+
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+        current_modified_time = os.path.getmtime(event.src_path)
+        if self._modified_times.get(event.src_path) == current_modified_time:
+            return
+        self._modified_times[event.src_path] = current_modified_time
+
+        logger.info(f'File {event.src_path} has been modified')
+        with open(event.src_path, 'r') as f:
+            code = f.read()
+        parser = LexicalParser(code)
+        tokens = parser.parse()
+        
+        token_filename = self.output_path + change_file_extension(event.src_path.split('/')[-1], 'out')
+        save_tokens_to_file(tokens, token_filename)
+        
+        symbol_filename = self.output_path + change_file_extension(event.src_path.split('/')[-1], 'sym')
+        save_symbols_to_file(get_symbol_list(tokens), symbol_filename)
+        logger.info('Updated')
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser(description='Lexical Parser')
+    
+    ap.add_argument('filename', type=str, help='input file')
+    ap.add_argument('-v', '--verbose', action='store_true', help='verbose mode')
+    ap.add_argument('-o', '--output', type=str, help='output file path', default='./outputs/')
+    ap.add_argument('-w', '--watch', action='store_true', help="watch target file then parse in realtime")
+    
+    args = ap.parse_args()
+    filename = args.filename
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+        logger.debug("Verbose mode enabled.")
+    
+    code = read_code(filename)
+    
+    if args.watch:
+        path = "./tmp/input"
+
+        event_handler = WatchHandler(args.output)
+        observer = Observer()
+        observer.schedule(event_handler, filename)
+
+        logger.info(f"Start monitoring {filename} for changes...")
+        observer.start()
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+            observer.join()
+            sys.exit(0)
+    
+    # start to parse code
+    parser = LexicalParser(code)
+    tokens = parser.parse()
+    logger.info('Parse done.')
+    
+    # Save tokens to file
+    token_filename = args.output + change_file_extension(filename.split('/')[-1], 'out')
+    save_tokens_to_file(tokens, token_filename)
+    
+    # print symbol table
+    symbol_list = get_symbol_list(tokens)
     
     symbol_filename = args.output + change_file_extension(filename.split('/')[-1], 'sym')
-    with open(symbol_filename, "w") as f:
-        for k in keyword_list:
-            f.write(f'KEYWORD {k}\n')
-        for i in identifier_list:
-            f.write(f'IDENTIFIER {i}\n')
-        for n in number_list:
-            f.write(f'NUMBER {n}\n')
-    logger.info(f"Symbol table saved to {symbol_filename}")
-            
-    if args.verbose:
-        logger.debug("Symbol Table: ")
-        symbol_table = rich.table.Table(title="Symbol Table")
-        symbol_table.add_column("Type", style="magenta")
-        symbol_table.add_column("Value", style="green")
-        for k in keyword_list:
-            symbol_table.add_row("KEYWORD", k)
-        for i in identifier_list:
-            symbol_table.add_row("IDENTIFIER", i)
-        for n in number_list:
-            symbol_table.add_row("NUMBER", n)
-
-    if args.verbose:
-        logger.debug("Showing tables:")
-        syntax = Syntax(code, "c", line_numbers=True, word_wrap=True, code_width=60)
-        panel = Panel(syntax, title="Source code")
-        columns = Columns([panel, token_table, symbol_table])
-        print(columns)
+    save_symbols_to_file(symbol_list, symbol_filename)
         
-
+    logger.info("Showing results:")
+    syntax = Syntax(code, "c", line_numbers=True, word_wrap=True, code_width=60)
+    panel = Panel(syntax, title="Source code")
+    columns = Columns([panel, create_token_table(tokens), create_symbol_table(symbol_list)])
+    print(columns)
+        
     logger.info("Done. exiting...")
+    sys.exit(0)
