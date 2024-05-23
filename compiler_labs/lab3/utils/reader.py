24a80bc76886cf6e5731736fe9395366882f27b7
zenor0 <zenor0@outlook.com>
2024-05-23 19:45:33 +0800

    feat: sematic grammar file reader

--- compiler_labs/lab3/utils/reader.py
+++ compiler_labs/lab3/utils/reader.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append('..')
 
-from lab2.models import Production, Symbol
+from compiler_labs.lab3.models import Snippet
+from compiler_labs.lab2.models import Production, Symbol
 import re
 
 PRODUCTION_RE = r'\s*(\S+)\s*->\s*(.+)\n'
@@ -11,9 +12,9 @@ REPLACE_RE = r"\n\1 -> \2 \n\1 -> \4\n"
 
 
 FUNC_DEFINITION_RE = r"'''([\s\S]*)'''"
-FUNC_SNIPPET_RE = r"<(.+)>"
+FUNC_SNIPPET_RE = r"<<(.*?)>>"  # non-greedy match
 
-def read_semantic_grammar(raw : str) -> list[Production]:
+def read_semantic_grammar(raw : str, no_snippet=False) -> list[Production]:
     # remove comments
     raw = re.sub(COMMENT_RE, "", raw)
     
@@ -27,16 +28,28 @@ def read_semantic_grammar(raw : str) -> list[Production]:
     if match:
         exec(match.group(1), functions)
     
-    for key, value in functions.items():
-        print(key)   
+    if no_snippet:
+        raw = re.sub(FUNC_SNIPPET_RE, "", raw)
     
     match = re.findall(PRODUCTION_RE, raw)
     # create a list of productions
     productions = []
     for prod in match:
         head = Symbol(prod[0])
-        body = [Symbol(x) for x in prod[1].split()]
+        body_str = prod[1]
+        match = re.findall(FUNC_SNIPPET_RE, body_str)
+        code_snippets = [x for x in match]
+        body_str = re.sub(FUNC_SNIPPET_RE, "<CODE>", body_str)
+        
+        body = []
+        cnt = 0
+        for x in body_str.split():
+            if x != '<CODE>':
+                body.append(Symbol(x))
+            else:
+                body.append(Snippet(code_snippets[cnt]))
+                cnt += 1
         productions.append(Production(head, body))
     
     # de-duplicate
-    return [x for i, x in enumerate(productions) if productions.index(x) == i]
+    return [x for i, x in enumerate(productions) if productions.index(x) == i], functions
